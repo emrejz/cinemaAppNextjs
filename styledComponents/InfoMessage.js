b099b43fc021818ecfd8b9b6bf511a5e46107ba8
emrejz <emre.kayacan@msn.com>
2019-11-13 03:21:44 +0300

    ui edited

--- styledComponents/InfoMessage.js
+++ styledComponents/InfoMessage.js
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 export const InfoMessage = styled.h1(
   props => `
-  color: ${props.error ? "red" : "gray"};
+  color: ${props.error ? "red" : "black"};
   font-size: ${props.lg ? "50px" : props.md ? "40px" : "30px"};
   text-align: center;
-  margin-top: 30%;
+  margin-top: 20%;
     
   `
 );
