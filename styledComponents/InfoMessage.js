0f90805455cf68501ee20ba0ab61303e70e408a7
emre kayacan <emre.kayacan@msn.com>
2020-08-02 14:27:06 +0300

    formated by global prettier, added codition

--- styledComponents/InfoMessage.js
+++ styledComponents/InfoMessage.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 export const InfoMessage = styled.h1(
-  props => `
-  color: ${props.error ? "red" : "black"};
+  (props) => `
+  color: ${props.error ? "red" : props.load ? "darkorange" : "black"};
   font-size: ${props.lg ? "50px" : props.md ? "40px" : "30px"};
   text-align: center;
   margin-top: 20%;
