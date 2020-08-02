3521a035b4cd5ec1c587703874bd0731379d156c
emre kayacan <emre.kayacan@msn.com>
2020-08-02 14:13:51 +0300

    added page links on navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,30 @@
-import { useRouter } from "next/router";
 import { Navbar } from "../styledComponents/Navbar";
+import { useRouter } from "next/router";
+import Link from "next/link";
 
-export default () => {
+const MyNavbar = () => {
   const router = useRouter();
   return (
     <Navbar>
-      <a onClick={() => router.pathname != "/" && router.push("/")}>Home</a>
+      <Link href="/">
+        <a className={router.pathname === "/" ? "active" : null}>Home</a>
+      </Link>
+      <Link href="/movies/static">
+        <a className={router.pathname === "/movies/static" ? "active" : null}>
+          Static
+        </a>
+      </Link>
+      <Link href="/movies/dynamic">
+        <a className={router.pathname === "/movies/dynamic" ? "active" : null}>
+          Dynamic
+        </a>
+      </Link>
+      <Link href="/movies/isr">
+        <a className={router.pathname === "/movies/isr" ? "active" : null}>
+          Isr
+        </a>
+      </Link>
     </Navbar>
   );
 };
+export default MyNavbar;
