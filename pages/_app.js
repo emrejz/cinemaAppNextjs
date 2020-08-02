0fef4f0e5a97c34804c286019e66906c8a2d992d
emre kayacan <emre.kayacan@msn.com>
2020-08-02 14:19:00 +0300

    fixed _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,55 @@
 import React from "react";
-import App from "next/app";
+import Router from "next/router";
 import Head from "next/head";
-import { GlobalState } from "../store/GlobalState";
+import App from "next/app";
+import { SWRConfig } from "swr";
+import fetch from "isomorphic-unfetch";
+import "../public/static/style.css";
 import Navbar from "../components/Navbar";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
 
-class MyApp extends App {
+let currentPath = "/";
+Router.onRouteChangeStart = (e) => {
+  // console.log('onRouteChnageStart triggered');
+
+  if (currentPath != e) {
+    NProgress.start();
+    currentPath = e;
+  }
+};
+
+Router.onRouteChangeComplete = () => {
+  // console.log('onRouteChnageComplete triggered');
+  NProgress.done();
+};
+
+Router.onRouteChangeError = () => {
+  // console.log('onRouteChnageError triggered');
+  NProgress.done();
+};
+
+export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
+
     return (
       <>
         <Head>
           <title>cinema Next.js</title>
-          <link href="/static/style.css" rel="stylesheet" />
+          <link rel="icon" type="image/png" href="/static/favicon.png" />
           <link rel="icon" type="image/png" href="/static/favicon.png" />
         </Head>
-        <GlobalState>
+
+        <SWRConfig
+          value={{
+            fetcher: (url) => fetch(url).then((r) => r.json()),
+          }}
+        >
           <Navbar />
           <Component {...pageProps} />
-        </GlobalState>
+        </SWRConfig>
       </>
     );
   }
 }
-
-export default MyApp;
